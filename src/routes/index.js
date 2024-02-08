0bfa105845abc464520bf32da4ab50e796154ad0
boztas13 <berk_oztas35@hotmail.com>
2024-02-08 16:49:43 +0300

    QR Kod ve Yetkilendirme Harici Ogretmen sayfasi ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,12 @@ const router = express.Router();
 const authRoutes = require('./authRoutes');
 const studentHomePage = require('./studentHomePage'); // Eğer bu rota varsa ve kullanılacaksa
 const courses = require('./courses');
+const qr= require('./Qrcode')
 
 // Tüm alt rotaları burada birleştirin
 router.use(authRoutes);
 router.use(studentHomePage); // Eğer bu rota varsa ve kullanılacaksa
 router.use(courses);
+router.use(qr);
 
 module.exports = router;  // Boşluklar kaldırıldı
