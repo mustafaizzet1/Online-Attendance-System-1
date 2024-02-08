086261c7f23b1c3e11a767e861fd06068bda0c41
boztas13 <berk_oztas35@hotmail.com>
2024-02-08 14:57:33 +0300

    Course Controller and Teacher Pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 const authRoutes = require('./authRoutes');
 const studentHomePage = require('./studentHomePage'); // Eğer bu rota varsa ve kullanılacaksa
+const courses = require('./courses');
 
 // Tüm alt rotaları burada birleştirin
 router.use(authRoutes);
 router.use(studentHomePage); // Eğer bu rota varsa ve kullanılacaksa
+router.use(courses);
 
 module.exports = router;  // Boşluklar kaldırıldı
