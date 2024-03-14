a9e8aa8958a99cb9e7c6e5333c90a067f08e42c5
mustafaizzet1 <ymustafaizzet@gmail.com>
2024-03-14 17:13:02 +0300

    com

--- src/controllers/teachermenuController.js
+++ src/controllers/teachermenuController.js
@@ -13,27 +13,34 @@ let OGRETIM_DONEMI_KODU = 303;
 
 exports.courses = async (req, res) => {
     try {
-
-
+        // Fetch Lectureinfos
         const lectureinfos = await Lectureinfo.findAll({
-            where: { OGRETIM_DONEMI_KODU: OGRETIM_DONEMI_KODU, KULLANICI_KODU: "mehmet.ozcanhan@deu.edu.tr" }, //req.session.user.KULLANICI_KODU },
+            where: { OGRETIM_DONEMI_KODU: OGRETIM_DONEMI_KODU, KULLANICI_KODU: "mehmet.ozcanhan@deu.edu.tr" },
             include: [
                 { 
-                    model: Lecture, // Include Lecture model first
-                   
-                    include: [
-                        { model: Branch } // Then include Branch model within Lecture
-                    ]
+                    model: Lecture
                 }
             ]
         });
 
+        // Fetch corresponding Branch information based on matching SUBE_KODU and OGR_BIRIM_KODU
+        const lectureinfosWithBranch = await Promise.all(lectureinfos.map(async (lectureinfo) => {
+            const branch = await Branch.findOne({
+                where: {
+                    SUBE_KODU: lectureinfo.SUBE_KODU,
+                    OGR_BIRIM_KODU: lectureinfo.OGR_BIRIM_KODU,
+                   // Additional condition if needed
+                }
+            });
+            return {
+                lectureinfo,
+                branch
+            };
+        }));
 
-
-
-
-        res.json(lectureinfos);
+        res.json(lectureinfosWithBranch);
     } catch (error) {
-        res.status(404).json({ message: 'Naneyi yidin' });
+        res.status(404).json({ message: 'Error occurred' });
     }
 };
+
